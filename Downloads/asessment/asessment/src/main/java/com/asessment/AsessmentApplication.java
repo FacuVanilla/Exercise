d90b0ef4332bfbd0b108ff5daff642446a9039a1
Seun <olagunjuoluwaseun1@gmail.com>
2023-03-29 11:15:59 +0100

    IDK

---
+++ Downloads/asessment/asessment/src/main/java/com/asessment/AsessmentApplication.java
@@ -0,0 +1,13 @@
+package com.asessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsessmentApplication.class, args);
+	}
+
+}
